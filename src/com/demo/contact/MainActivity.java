af1daa09f01485a6d18e5f1bffa017ce62c0db74
devtestvj <Vijay_Shyamasundar@ISC-2103s-MBP.lan>
2015-06-17 21:15:26 -0400

    Test10

--- src/com/demo/contact/MainActivity.java
+++ src/com/demo/contact/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity
 		 
 					// set dialog message
 					alertDialogBuilder
-						.setMessage("Hello Rah!")
+						.setMessage("Hello World!")
 						.setCancelable(false)
 						.setPositiveButton("Ok",null);
 						
