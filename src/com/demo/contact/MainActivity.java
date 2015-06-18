1371868d5c41f44565baf45cc646820428ae5792
devtestvj <Vijay_Shyamasundar@ISC-2103s-MacBook-Pro.local>
2015-06-18 08:51:13 -0400

    Test20

--- src/com/demo/contact/MainActivity.java
+++ src/com/demo/contact/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity
 		 
 					// set dialog message
 					alertDialogBuilder
-						.setMessage("Hello World!")
+						.setMessage("Hello World!!")
 						.setCancelable(false)
 						.setPositiveButton("Ok",null);
 						
